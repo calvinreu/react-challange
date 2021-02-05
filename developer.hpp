8f776a8b68f780f395e5e3dbb327fbbb87c4087f
calvinreu <j.calvin.reu@protonmail>
2021-02-05 16:41:04 +0100

    .

--- developer.hpp
+++ developer.hpp
@@ -1,19 +1,47 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <set>
 
-class developer
+
+enum field_type{
+    empty,
+    manager,
+    developer
+}
+
+class developer_manager
 {
 private:
-    float company, bonus, skills;
+    int bonus;
+    std::set<std::string> skills;
+    std::string company;
+    int bonus_yield(int other_bonus) const {return bonus*other_bonus;}
+    int skill_yield(const std::set<std::string> &other) const;  
 public:
-    developer(float company, float bonus, float skills);
-    float get_company() const {return company;}
+    developer_manager(std::string company, int bonus, std::set<std::string> skills);
+    field_type field;
+    std::string get_company() const {return company;}
     float get_bonus()   const {return bonus;}
-    float get_skills()  const {return skills;}
-    ~developer();
+    std::set<std::string> get_skills()  const {return skills;}
+
+    int get_value(const developer_manager &other) { return this->bonus_yield(other.get_bonus()) + this->skill_yield(other.get_skills()); }
+
+    ~developer_manager();
 };
 
-developer::developer(float company, float bonus, float skills) : company(company), bonus(bonus), skills(skills) {}
+int developer_manager::skill_yield(const std::set<std::string> &other_skills) const {
+    auto j = skills.begin();
+    int value;
+    for (auto i = other_skills.begin(); i != other_skills.end(); i++) {
+        for (j; *j < *i; j++) {
+            if 
+        }
+    }
+}
 
-developer::~developer()
+developer_manager::developer(std::string company, int bonus, std::set<std::string> skills) : company(company), bonus(bonus), skills(skills) {}
+
+developer_manager::~developer()
 {
 }
